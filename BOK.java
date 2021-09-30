9337f808f0ee83daeb29efd55b486c037487604d
Harsh Kotwani <shop2428804@gmail.com>
2021-09-30 22:31:34 +0530

    wanted to change the name.

--- BOK.java
+++ BOK.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class BankOfKotwani {
+public class BOK {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int sentinal = 0;
